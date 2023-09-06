5c78ed363fafc957a49ccdfaaa3b52fb289c5b04
rmdettmar <rmdettmar@gmail.com>
2023-09-06 12:05:03 -0400

    typescript renderer parameter rename (rectangle->shape)

--- typescript/src/renderer.ts
+++ typescript/src/renderer.ts
@@ -1,9 +1,9 @@
 import { Shape } from "./shapes/shape.js"
 
-function newRenderer(rectangle: Shape) {
+function newRenderer(shape: Shape) {
     return {
         draw() {
-            const area: number = rectangle.computeArea()
+            const area: number = shape.computeArea()
             console.log("Shape drawn\n" + "Its area is " + area)
         }
     }
